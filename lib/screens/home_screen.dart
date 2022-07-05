aa07040846d8749987ec3507c30ee18ecf346d11
Odinachi David <odinachidavid@ganil.com>
2022-07-05 15:55:44 +0100

    bug fix

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
+
   static User? user = FirebaseAuth.instance.currentUser;
 
   @override
